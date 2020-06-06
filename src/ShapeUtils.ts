2dea71172a7c97422755f6f188384cad93537963
Roman Vereshchagin <info@sibvrv.com>
2020-06-07 04:44:53 +0700

    isPointInPoly method

--- src/ShapeUtils.ts
+++ src/ShapeUtils.ts
@@ -20,3 +20,27 @@ export function polygonArea(vertices: Vector2[]) {
  * @param points
  */
 export const isClockwise = (points: Vector2[]) => polygonArea(points) > 0;
+
+/**
+ * Is Point In Poly
+ * ray-casting algorithm based on http://www.ecse.rpi.edu/Homepages/wrf/Research/Short_Notes/pnpoly.html
+ * @param x
+ * @param y
+ * @param vertices
+ */
+export function isPointInPoly(x: number, y: number, vertices: Vector2[]) {
+  let inside = false;
+  for (let i = 0, j = vertices.length - 1; i < vertices.length; j = i++) {
+    const xi = vertices[i].x;
+    const yi = vertices[i].y;
+
+    const xj = vertices[j].x;
+    const yj = vertices[j].y;
+
+    const intersect = ((yi > y) !== (yj > y)) && (x < (xj - xi) * (y - yi) / (yj - yi) + xi);
+    if (intersect) {
+      inside = !inside;
+    }
+  }
+  return inside;
+}
